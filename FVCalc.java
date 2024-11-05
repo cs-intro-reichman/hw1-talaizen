f298840bd229896a8e8268bdfe35665e46b139d8
tal.aizenkraft <tal.aizenkraft@tufin.com>
2024-11-05 22:30:19 +0200

    fixed test expected output corrections ex 2

--- FVCalc.java
+++ FVCalc.java
@@ -5,6 +5,6 @@ public class FVCalc {
 		double interest = Double.parseDouble(args[1]);
 		int years = Integer.parseInt(args[2]);
 		double futureValue = currentValue * Math.pow((1 + interest / 100), (double) years);
-		System.out.println("After " + years + " years, $" + currentValue + " saved at " + interest + "% will yield $" +  (int) futureValue);
+		System.out.println("After " + years + " years, a $" + currentValue + " saved at " + interest + "% will yield $" +  (int) futureValue);
 	}
 }
