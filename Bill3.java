fb1348ebb80278ed17b30cfe4657a331b88f2cf2
tal.aizenkraft <tal.aizenkraft@tufin.com>
2024-11-05 22:23:31 +0200

    fixed test expected output corrections

--- Bill3.java
+++ Bill3.java
@@ -7,6 +7,6 @@ public class Bill3 {
 		String name3 = args[2];
 		int bill = Integer.parseInt(args[3]);
 		double devidedBill = Math.ceil((double) bill / 3);
-		System.out.println("Daer " + name1 + ", " + name2 + ", and " + name3 + ": " + "pay " + devidedBill + " Shekels each"); 
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": " + "pay " + devidedBill + " Shekels each"); 
 	}
 }
