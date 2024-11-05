051c438641a7eed4b330f0da30f155933d629463
tal.aizenkraft <tal.aizenkraft@tufin.com>
2024-11-05 18:51:21 +0200

    excersize 1 - Bill Three

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,7 @@ public class Bill3 {
 		String name2 = args[1];
 		String name3 = args[2];
 		int bill = Integer.parseInt(args[3]);
-		double devided_bill = Math.ceil((double) bill / 3);
-		System.out.println("Daer " + name1 + ", " + name2 + ", and " + name3 + ": " + "pay " + devided_bill + " Shekels each"); 
+		double devidedBill = Math.ceil((double) bill / 3);
+		System.out.println("Daer " + name1 + ", " + name2 + ", and " + name3 + ": " + "pay " + devidedBill + " Shekels each"); 
 	}
 }
