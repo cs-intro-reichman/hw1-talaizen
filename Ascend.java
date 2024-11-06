f696a32876005c6d6715a3634ba84196b7190fd1
tal.aizenkraft <tal.aizenkraft@tufin.com>
2024-11-06 21:59:49 +0200

    Excercise 4 - Ascending order

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,18 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int limit = Integer.parseInt(args[0]); 
+		int randomInt1 = (int) (Math.random() * limit);
+		int randomInt2 = (int) (Math.random() * limit);
+		int randomInt3 = (int) (Math.random() * limit);
+
+		// find min, max and middle values
+		int min = Math.min((Math.min(randomInt1, randomInt2)), randomInt3);
+		int max = Math.max((Math.max(randomInt1, randomInt2)), randomInt3);
+		int middle = (randomInt1 + randomInt2 + randomInt3) - (max + min);
+
+		//prints the generated random numbers then again in ascending order
+		System.out.println(randomInt1 + " " + randomInt2 + " " + randomInt3);
+		System.out.println(min + " " + middle + " " + max);
 	}
 }
